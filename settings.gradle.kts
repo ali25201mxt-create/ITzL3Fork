dba01155f41f2b5a3078d63f6406b39da711b7a7
ali25201mxt-create <ali25201mxt@gmail.com>
2026-06-18 14:44:03 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,7 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ZalithLauncher"
+rootProject.name = "ITz3lFORK"
 include(":ZalithLauncher")
 include(":LWJGL")
 include(":LayerController")
